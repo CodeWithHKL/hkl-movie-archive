2df471637366cc7889809f586587a44fb2e3c197
HKL <229150515+CodeWithHKL@users.noreply.github.com>
2026-03-08 19:14:38 +0800

    Sort Best Picks according to code sequence

--- lib/collections.ts
+++ lib/collections.ts
@@ -7,35 +7,38 @@ export interface Collection {
   movies: Movie[]; // Hardcoded list
 }
 
+const getMoviesByIds = (ids: string[]) => 
+  ids.map(id => mockMovies.find(m => m.id === id)).filter((m): m is Movie => !!m);
+
 export const BEST_PICKS_COLLECTIONS: Collection[] = [
   {
     title: "All-Time Favorites",
     description: "My personal best of the best.",
-    movies: mockMovies.filter(m => m.id === '152' || m.id === '69' || m.id === '51' || m.id === '93') // Add your specific IDs here
+    movies: getMoviesByIds(['152', '69', '51', '93'])
   },
   {
     title: "Action Masterpiece",
     description: "Movies I rated significantly higher than the public.",
-    movies: mockMovies.filter(m => m.id === '93' || m.id === '156')
+    movies: getMoviesByIds(['93', '156'])
   },
   {
     title: "Visual Elite",
     description: "Movies visual that are ahead of it's time.",
-    movies: mockMovies.filter(m => m.id === '135' || m.id === '136' || m.id === '99' || m.id === '31' || m.id === '152' )
+    movies: getMoviesByIds(['135', '136', '99', '31', '152'])
   },
   {
     title: "Storyline Genius",
     description: "The best adrenaline-fueled favorites.",
-    movies: mockMovies.filter(m => m.id === '203' || m.id === '140' || m.id === '117' || m.id === '104' || m.id === '74')
+    movies: getMoviesByIds(['203', '140', '117', '104', '74'])
   },
   {
     title: "Impactful",
     description: "The best adrenaline-fueled favorites.",
-    movies: mockMovies.filter(m => m.id === '204' || m.id === '120' || m.id === '91' || m.id === '100' || m.id === '119')
+    movies: getMoviesByIds(['204', '120', '91', '100', '119'])
   },
   {
     title: "Underrated",
     description: "Movies I rated significantly higher than the public.",
-    movies: mockMovies.filter(m => m.id === '8' || m.id === '14' || m.id === '99')
+    movies: getMoviesByIds(['8', '14', '99'])
   }
 ];
